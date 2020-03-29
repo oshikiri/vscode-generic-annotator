9b94a498b3bba894dc3bc087081a4795245015e4
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-29 17:43:18 +0900

    Fix test case

--- client/src/test/diagnostics.test.ts
+++ client/src/test/diagnostics.test.ts
@@ -1,20 +1,24 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 import * as vscode from 'vscode';
 import * as assert from 'assert';
 import { getDocUri, activate } from './helper';
 
 suite('Should get diagnostics', () => {
-	const docUri = getDocUri('diagnostics.txt');
+	const docUri = getDocUri('imbalance.ledger');
 
 	test('Diagnoses uppercase texts', async () => {
 		await testDiagnostics(docUri, [
-			{ message: 'ANY is all uppercase.', range: toRange(0, 0, 0, 3), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' },
-			{ message: 'ANY is all uppercase.', range: toRange(0, 14, 0, 17), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' },
-			{ message: 'OS is all uppercase.', range: toRange(0, 18, 0, 20), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' }
+			{
+				message: 'imbalanced transaction, (total amount) = -1800 JPY',
+				range: toRange(0, 0, 0, 80),
+				severity: vscode.DiagnosticSeverity.Error,
+				source: 'ledgerlint',
+			},
+			{
+				message: 'imbalanced transaction, (total amount) = -20000 USD + 2000 JPY',
+				range: toRange(4, 0, 4, 80),
+				severity: vscode.DiagnosticSeverity.Error,
+				source: 'ledgerlint',
+			},
 		]);
 	});
 });
