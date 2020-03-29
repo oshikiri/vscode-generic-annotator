68260782081a66455055936bd63efa94278b30f0
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-29 17:57:03 +0900

    Fix file path issue at runLedgerLint

--- server/src/ledgerlint.ts
+++ server/src/ledgerlint.ts
@@ -21,13 +21,13 @@ const exec = util.promisify(require('child_process').exec);
 export async function runLedgerLint(absPath: string): Promise<LedgerLintError[]> {
 	const { stdout } = await exec(`ledgerlint -f $(realpath --relative-to=. ${absPath})`);
 	const lines = String(stdout).split('\n');
-	const errors: LedgerLintError[] =[];
+	const errors: LedgerLintError[] = [];
 	lines.forEach(line => {
-		const m = line.match(/^([\w\d\.]+):(\d+)\s+(.+)$/);
+		const m = line.match(/^([\w\d\./]+):(\d+)\s+(.+)$/);
 		if (m === null) {
 			return;
 		}
-		const error = new LedgerLintError(m[1], Number(m[2])-1, m[3]);
+		const error = new LedgerLintError(absPath, Number(m[2])-1, m[3]);
 		errors.push(error);
 	});
 
