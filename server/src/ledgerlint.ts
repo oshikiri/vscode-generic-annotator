742ec8951f8112da225272e88a47beae31f8f692
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-30 01:06:25 +0900

    Use dummy stdout

--- server/src/ledgerlint.ts
+++ server/src/ledgerlint.ts
@@ -20,7 +20,8 @@ const exec = util.promisify(require('child_process').exec);
 export async function runLedgerLint(absPath: string): Promise<LedgerLintError[]> {
 	// throw Error(absPath); // なぜかここまで来てない
 	console.log({absPath});
-	const { stdout } = await exec(`ledgerlint -f $(realpath --relative-to=. ${absPath})`);
+	// const { stdout } = await exec(`ledgerlint -f $(realpath --relative-to=. ${absPath})`);
+	const stdout = 'client/testFixture/imbalance.ledger:1 imbalanced transaction, (total amount) = -1800 JPY\nclient/testFixture/imbalance.ledger:5 imbalanced transaction, (total amount) = -18000 JPY\n';
 	console.log({stdout});
 	const lines = String(stdout).split('\n');
 	const errors: LedgerLintError[] = [];
