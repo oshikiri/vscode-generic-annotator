767382fd7c52b2950fa7a83bf54d4811ec55f12d
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-30 22:22:49 +0900

    Refactor

--- server/src/exec.ts
+++ server/src/exec.ts
@@ -11,9 +11,6 @@ export async function execPromise(command: string): Promise<string> {
 		const r = await exec(command);
 		stdout = r.stdout;
 		stderr = r.stderr;
-
-		// const stdout = 'client/testFixture/imbalance.ledger:1 imbalanced transaction, (total amount) = -1800 JPY\nclient/testFixture/imbalance.ledger:5 imbalanced transaction, (total amount) = -18000 JPY\n';
-		// console.log({stdout});
 		return stdout;
 	} catch (err) {
 		throw new ExecError(`command = ${command}\nerr = ${err}\nstderr = ${stderr}`);
--- server/src/ledgerlint.ts
+++ server/src/ledgerlint.ts
@@ -16,8 +16,8 @@ export class LedgerLintError {
 }
 
 export async function runLedgerLint(absPath: string): Promise<LedgerLintError[]> {
-	console.log({absPath});
-	const stdout = await execPromise(`/home/runner/.local/bin/ledgerlint -f $(realpath --relative-to=. ${absPath})`);
+	const command = `/home/runner/.local/bin/ledgerlint -f $(realpath --relative-to=. ${absPath})`;
+	const stdout = await execPromise(command);
 	const lines = String(stdout).split('\n');
 	const errors: LedgerLintError[] = [];
 	lines.forEach(line => {
