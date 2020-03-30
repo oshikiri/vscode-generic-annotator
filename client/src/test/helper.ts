1506179c8820182f90fbb82f6571c84b13ed6ed1
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-30 22:57:38 +0900

    Rename vscode-ledgerlint to ledgerlint

--- client/src/test/helper.ts
+++ client/src/test/helper.ts
@@ -13,7 +13,7 @@ export let platformEol: string;
 
 export async function activate(docUri: vscode.Uri) {
 	// The extensionId is `publisher.name` from package.json
-	const ext = vscode.extensions.getExtension('oshikiri.vscode-ledgerlint')!;
+	const ext = vscode.extensions.getExtension('oshikiri.ledgerlint')!;
 	await ext.activate();
 	try {
 		doc = await vscode.workspace.openTextDocument(docUri);
