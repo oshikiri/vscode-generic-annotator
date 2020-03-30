c78f29f5ccb21969e1bc4e8fffa4ecfd2680a6d4
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-30 22:24:34 +0900

    Fix CI error (3) (#4)
    * Split exec
    
    * Add @types/jest
    
    * Use chai-as-promised
    
    * Add tests for execPromise
    
    * Use $HOME/.local/bin/ledgerlint instead ledgerlint
    
    * Do not use $HOME
    
    * Add ledgerlint-h to test step
    
    * Revert "Add ledgerlint-h to test step"
    
    This reverts commit e2423842bd4aba99cbbffb4b28a51afc18ffd3d9.
    
    * Add error message
    
    * Catch error of ledgerlint
    
    * Fix ledgerlint path
    
    * Use ExecError instead of Exec
    
    * Split  testing process
    
    * Remove $PATH tests
    
    * Skip exec test
    
    * Revert tsconfig.json
    
    * Refactor

---
+++ server/src/exec.ts
@@ -0,0 +1,18 @@
+import * as util from 'util';
+
+const exec = util.promisify(require('child_process').exec);
+
+class ExecError extends Error {}
+
+export async function execPromise(command: string): Promise<string> {
+	let stdout: string;
+	let stderr: string = '';
+	try {
+		const r = await exec(command);
+		stdout = r.stdout;
+		stderr = r.stderr;
+		return stdout;
+	} catch (err) {
+		throw new ExecError(`command = ${command}\nerr = ${err}\nstderr = ${stderr}`);
+	}
+}
---
+++ server/test/exec.test.ts
@@ -0,0 +1,24 @@
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const { execPromise } = require('./../src/exec');
+
+describe('exec', () => {
+	context('when an echo command is passed', () => {
+		it('returns a fulfilled promise', async ()=>{
+			return expect(execPromise('echo Hi')).eventually.equal('Hi\n');
+		});
+	});
+	context('when a failing command is passed', () => {
+		it('returns rejected promise', async ()=>{
+			return expect(execPromise('test 0 -eq 1')).eventually.rejected;
+		});
+	});
+	context('when ledgerlint -h command is passed', () => {
+		it.skip('returns a fulfilled promise', () => {
+			return expect(execPromise('/home/runner/.local/bin/ledgerlint -h || test $? -eq 2')).eventually.be.a('string').and.match(/^Usage of ledgerlint/);
+		});
+	});
+});
