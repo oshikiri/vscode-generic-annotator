9c852b5e6aa8c879956eee986862ce5a20109d08
Takamasa Oshikiri <mail@oshikiri.org>
2020-03-30 21:29:25 +0900

    Add error message

--- server/src/exec.ts
+++ server/src/exec.ts
@@ -3,12 +3,17 @@ import * as util from 'util';
 const exec = util.promisify(require('child_process').exec);
 
 export async function execPromise(command: string): Promise<string> {
+	let stdout: string;
+	let stderr: string = '';
 	try {
-		const { stdout } = await exec(command);
+		const r = await exec(command);
+		stdout = r.stdout;
+		stderr = r.stderr;
+
 		// const stdout = 'client/testFixture/imbalance.ledger:1 imbalanced transaction, (total amount) = -1800 JPY\nclient/testFixture/imbalance.ledger:5 imbalanced transaction, (total amount) = -18000 JPY\n';
 		// console.log({stdout});
 		return stdout;
-	} catch {
-		throw Error(`failed: ${command}`);
+	} catch (err) {
+		throw Error(`command = ${command}\nerr = ${err}\nstderr = ${stderr}`);
 	}
 }
