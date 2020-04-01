82810e2511ab1f4f2800093d378a2db07f7d036f
Takamasa Oshikiri <mail@oshikiri.org>
2020-04-01 23:12:27 +0900

    ???

--- client/src/test/diagnostics.test.ts
+++ client/src/test/diagnostics.test.ts
@@ -26,7 +26,6 @@ suite('diagnostics tests', () => {
 		const docUri = getDocUri('unknown-account.ledger');
 		await activate(docUri);
 
-		assert.equal({config: vscode.workspace.getConfiguration()}, {});
 		await vscode.workspace.getConfiguration().update('ledgerlint.accountsPath', 'testFixture/accounts.txt');
 		assert.equal({config: vscode.workspace.getConfiguration()}, {});
 
