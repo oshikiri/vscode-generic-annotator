1f4808c76a34ffe095ba0ea5d873dcb355edd985
Takamasa Oshikiri <mail@oshikiri.org>
2022-01-05 23:08:25 +0900

    Bump ledgerlint to v1.1.1 (#30)
    * Use range from ledgerlint
    
    * Bump LEDGERLINT_VERSION to v1.1.1
    
    * Rename error_message to message
    
    * Empty commit

--- server/src/diagnostics.ts
+++ server/src/diagnostics.ts
@@ -1,11 +1,19 @@
 import { execPromise } from "./exec";
 import { Diagnostic, Range } from "vscode-languageserver";
 
+function convertToRange(obj: any): Range {
+  return Range.create(
+    obj["startLine"],
+    obj["startCharacter"],
+    obj["endLine"],
+    obj["endCharacter"]
+  );
+}
+
 function convertToDiagnostic(obj: any): Diagnostic {
-  const lineNumber = Number(obj["line_number"]) - 1;
   return {
-    range: Range.create(lineNumber, 0, lineNumber, 80),
-    message: obj["error_message"],
+    range: convertToRange(obj["range"]),
+    message: obj["message"],
     source: obj["source"],
     severity: obj["severity"],
   };
