ffdfa9f06ea294ccbf7bd68b08b1dc04aef9bb5c
Takamasa Oshikiri <mail@oshikiri.org>
2022-09-03 22:25:38 +0900

    Refactor client/

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,50 +1,64 @@
-import * as path from "path";
-import { workspace, ExtensionContext } from "vscode";
+import * as vscode from "vscode";
+import { getDiagnostics } from "./diagnostics";
 
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-  TransportKind,
-} from "vscode-languageclient";
-
-let client: LanguageClient;
-
-export function activate(context: ExtensionContext) {
-  const serverModule = context.asAbsolutePath(
-    path.join("server", "out", "server.js")
-  );
-  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-
-  const serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.ipc },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: debugOptions,
-    },
-  };
-
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: "file" }],
-    synchronize: {
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-    },
-  };
-
-  client = new LanguageClient(
-    "genericAnnotator",
-    "genericAnnotator",
-    serverOptions,
-    clientOptions
-  );
-
-  client.start();
+export function activate(context: vscode.ExtensionContext) {
+  const diagnostics =
+    vscode.languages.createDiagnosticCollection("diagnostics");
+  context.subscriptions.push(diagnostics);
+  subscribeToDocumentChanges(context, diagnostics);
 }
 
-export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined;
+// https://github.com/microsoft/vscode-extension-samples/blob/133fa26af64ba8760559c5a06299953673d60763/code-actions-sample/src/diagnostics.ts
+async function refreshDiagnostics(
+  doc: vscode.TextDocument,
+  diagnosticCollection: vscode.DiagnosticCollection
+): Promise<void> {
+  const path = doc.uri.toString().match(/file:\/\/(.+)/)?.[1];
+  if (path === undefined) {
+    return;
   }
-  return client.stop();
+
+  let diagnostics: vscode.Diagnostic[] = [];
+  const settings = vscode.workspace.getConfiguration(
+    "genericAnnotator",
+    doc.uri
+  );
+  for (const config of settings?.annotatorConfigurations) {
+    if (path.match(new RegExp(config.pathRegex))) {
+      const command = config.commandTemplate?.replace("${path}", path);
+      if (command) {
+        diagnostics = diagnostics.concat(await getDiagnostics(command));
+      }
+    }
+  }
+  diagnosticCollection.set(doc.uri, diagnostics);
+}
+
+function subscribeToDocumentChanges(
+  context: vscode.ExtensionContext,
+  diagnostics: vscode.DiagnosticCollection
+): void {
+  if (vscode.window.activeTextEditor) {
+    refreshDiagnostics(vscode.window.activeTextEditor.document, diagnostics);
+  }
+
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor) {
+        refreshDiagnostics(editor.document, diagnostics);
+      }
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument((e) =>
+      refreshDiagnostics(e.document, diagnostics)
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.workspace.onDidCloseTextDocument((doc) =>
+      diagnostics.delete(doc.uri)
+    )
+  );
 }
